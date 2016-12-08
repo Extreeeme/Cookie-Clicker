3714f287b5bb34f86923d6204f522a56d2e38e1c
Martin Dagostino <martindcontact@gmail.com>
2016-12-08 14:33:16 +0100

    Ajout compteur de questions

--- cookie.js
+++ cookie.js
@@ -1,16 +1,17 @@
-var reponse1;
-var reponse2;
-var reponse3;
-var reponse4;
-var reponse5;
+// un jour *un singe* *dislexique* ayant perdu
+// *la vue* qui s\'appelle *Jean-Moris*
+var animal;
+var maladie;
+var sens;
+var nom;
+var question = 4;
 
-reponse1 = prompt("Indiquez votre année de naissance");
-reponse2 = prompt("Dans quel lycée étiez vous ?");
-reponse3 = prompt("De quelle couleur est votre haut ?")
-reponse4 = prompt("A quoi ressemble votre mug ?")
-reponse5 = prompt("Quel est votre chanel Slack préférée ?")
-
-reponse1 = parseInt(reponse1)+10;
-reponse5 = '#'+reponse5
-
-alert('Il était une fois un bonhomme ' +reponse3 + ' qui étudiait au ' + reponse2 + ' et qui ressemblait à '+ reponse4 + '. Il avait '+reponse1+ ' cartes Pokemon et il adorait '+ reponse5);
+alert('Bonjour, nous allons te poser une série de quatre questions.');
+nom = prompt('Il vous reste '+question+ ' questions. Comment t\'appelles tu ?');
+question -= 1;
+maladie = prompt('Il vous reste '+question+ ' questions. Cite moi une malaide');
+question -= 1;
+animal = prompt('Il vous reste '+question+ ' questions. Quel est ton animal préféré');
+question -= 1;
+sens = prompt('Il vous reste '+question+ ' questions. Pouvez vous me citer un sens');
+alert('Un jour un/une '+animal+' atteins de/du '+maladie+' ayant perdu '+sens+' qui s\'appelait '+nom);
